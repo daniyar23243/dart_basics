4abe565420ebe7df7fe8fee205ffefe5521c44ff
chiefbaki <ik509331@gmail.com>
2025-04-04 14:12:10 +0600

    [init] - init commit

---
+++ lib/basics.dart
@@ -0,0 +1,29 @@
+//  Write a program that return true if either of two given integers is in the range 10..30 inclusive.
+
+bool isInRange(int a, int b) => false;
+
+// Write a program to check if a given string begins with "fix", except the 'f' can be any character or number.
+
+bool startsWithFixVariant(String str) => false;
+
+// Write a program to find the largest number among three given integers.
+
+int findLargest(int a, int b, int c) => 0;
+// Write a program that accept two integer values and test if they are both in the range 20..30 inclusive, or they are both in the range 30..40 inclusive.
+
+bool inSameRange(int a, int b) => false;
+
+// Write a program that accept two positive integer values and test whether the larger value is in the range 20..30 inclusive, or return 0 if neither is in that range.
+
+int largerInRange(int a, int b) => 0;
+
+// Write a program to test whether the last digit of the two given non-negative integer values are same or not.
+
+bool sameLastDigit(int a, int b) => false;
+
+// Write a program to convert the last three characters in upper case. If the string has less than 3 chars, lowercase whatever is there.
+
+String lastThreeUpper(String str) => '';
+// Write a program to check if the first instance of "a" in a given string is immediately followed by another "a".
+
+bool hasDoubleA(String str) => false;
