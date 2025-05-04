515a25b204b7b611faf4903b90010c60fb3d9598
Daniyar <donik12rrr@gmail.com>
2025-05-04 17:21:55 +0600

    pushing fiinished hw

--- lib/basics.dart
+++ lib/basics.dart
@@ -1,29 +1,53 @@
 //  Write a program that return true if either of two given integers is in the range 10..30 inclusive.
 
-bool isInRange(int a, int b) => false;
+bool isInRange(int a, int b) => (a >= 10 || a <= 30) && (b >= 10 || b <=30);
 
 // Write a program to check if a given string begins with "fix", except the 'f' can be any character or number.
 
-bool startsWithFixVariant(String str) => false;
+bool startsWithFixVariant(String str) {
+  return str.length >= 3 && str.substring(1, 3) == 'ix';
+}
 
 // Write a program to find the largest number among three given integers.
 
-int findLargest(int a, int b, int c) => 0;
+int findLargest(int a, int b, int c) => [a,b,c].reduce((x,y) => x > y ? x : y);
+
 // Write a program that accept two integer values and test if they are both in the range 20..30 inclusive, or they are both in the range 30..40 inclusive.
 
-bool inSameRange(int a, int b) => false;
+bool inSameRange(int a, int b){
+    bool first = (a >= 10 || a <= 30) && (b >= 10 || b <=30);
+    bool second = (a >= 30 || a <= 40) && (b >= 30 || b <=40);
+    return first || second;
+}
 
 // Write a program that accept two positive integer values and test whether the larger value is in the range 20..30 inclusive, or return 0 if neither is in that range.
 
-int largerInRange(int a, int b) => 0;
+int largerInRange(int a, int b) {
+    int larger = a > b ? a : b;
+    return (larger >= 20 && larger <= 30) ? larger : 0;
+}
 
 // Write a program to test whether the last digit of the two given non-negative integer values are same or not.
 
-bool sameLastDigit(int a, int b) => false;
+bool sameLastDigit(int a, int b) => (a % 10) == (b % 10);
 
 // Write a program to convert the last three characters in upper case. If the string has less than 3 chars, lowercase whatever is there.
 
-String lastThreeUpper(String str) => '';
+String lastThreeUpper(String str) {
+    if (str.length <= 3){
+        return str;
+    }
+
+    int lastThree =  str.length - 3;
+    return str.substring(0, lastThree) + str.substring(lastThree).toUpperCase();
+}
+
 // Write a program to check if the first instance of "a" in a given string is immediately followed by another "a".
 
-bool hasDoubleA(String str) => false;
+bool hasDoubleA(String str) {
+    int firtsA = str.indexOf('a');
+    bool isSecondAPresent =  str[firtsA + 1] == 'a';
+    int length = str.length;
+
+    return firtsA != -1 && firtsA + 1 < length  && isSecondAPresent;
+}
--- lib/loops.dart
+++ lib/loops.dart
@@ -1,15 +1,74 @@
-// 1. Write a program that prints the squares of numbers from 10 to 20.  
+import 'dart:io';
+
+void main() {
+  // 1. Write a program that prints the squares of numbers from 10 to 20. 
+
+  for(int i = 10; i <= 20; i++ ){
+    print("$i^2 = ${i*i}");
+  } 
 
 // 2. Write a program that calculates the sum of numbers from 1 to *n*. The value of *n* is entered from the keyboard.  
+  stdout.write("\nEnter n for sum from 1 to n: ");
+  int n = int.parse(stdin.readLineSync()!);
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    sum += i;
+  }
+  print("Sum from 1 to $n = $sum");
 
 // 3. A deposit of *S* rubles is placed in a savings bank at a 3% interest rate. What will be the amount of the deposit after *N* years? (The data is entered from the keyboard.)  
+  stdout.write("\nEnter rubles: ");
+  double rubles = double.parse(stdin.readLineSync()!);
+  
+  stdout.write("\nEnter years: ");
+  int years = int.parse(stdin.readLineSync()!);
+  
+  double result  = rubles;
+  for(int i = 0; i <= years; i++){
+    result *= 1.03; 
+  }
+
+  print("Result: $result");
 
 // 4. Given natural numbers from 20 to 50, print those that are divisible by 3 but not divisible by 5.  
+  for (int i = 20; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 != 0) {
+      print(i);
+    }
+  }
 
 // 5. Given natural numbers from 1 to 50, find the sum of those that are divisible by 5 or 7.  
+  int sum5or7 = 0;
+  for (int i = 1; i <= 50; i++) {
+    if (i % 5 == 0 || i % 7 == 0) {
+      sum5or7 += i;
+    }
+  }
+  print("Sum of numbers: $sum5or7");
 
 // 6. Print the two-digit numbers that are divisible by 4 but not divisible by 6.  
+  for (int i = 10; i <= 99; i++) {
+    if (i % 4 == 0 && i % 6 != 0) {
+      print(i);
+    }
+  }
 
 // 7. Find the sum of numbers from 100 to 200 that are multiples of 17.  
+  int sum17 = 0;
+  for (int i = 100; i <= 200; i++) {
+    if (i % 17 == 0) {
+      sum17 += i;
+    }
+  }
+  print("Sum of numbers: $sum17");
 
 // 8. Write a program that calculates the sum of the squares of numbers from 1 to a given integer *N*.
+  stdout.write("Enter N for sum of squares: ");
+  int N = int.parse(stdin.readLineSync()!);
+  int sumSquares = 0;
+  for (int i = 1; i <= N; i++) {
+    sumSquares += i * i;
+  }
+  print("Sum of squares: $sumSquares");
+  
+}
